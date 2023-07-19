dd07ce8822897ab8583df909a919021772f3105f
TheBarteQ <antonpodzr@gmail.com>
2023-07-19 10:20:43 -0400

    added hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv){
+	string name;
+	cout << "Input user_name":
+	cin >> name;
+	cout << "Hello world from " << name << endl;
+	return 0;
+}
