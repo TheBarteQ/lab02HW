7fd412b550f7b0a5e5649259570da968fd86e09f
TheBarteQ <TheBarteQ@github.com>
2023-07-23 12:59:40 -0400

    added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main(int argc, char* argv){
-	std::string name;
+	std::string name;// используем переменную типа strin
 	std::cout << "Input user_name":
-	std::cin >> name;
-	std::cout << "Hello world from " << name << endl;
+	std::cin >> name;// вводим своё имя в переменную
+	std::cout << "Hello world from " << name << endl;// выводим приветствие
 	return 0;
 }
